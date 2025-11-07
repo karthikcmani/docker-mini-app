24696d994d5ed3cd1b46c4c53ab3c29e6c3f10ad
karthik <aflalmathew1991@gmail.com>
2025-11-07 15:07:03 +0530

    use render backend URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
-      .then(res => res.json())
+    fetch("https://docker-mini-backend.onrender.com/api/hello")      .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error("Error fetching message:", err));
   }, []);
